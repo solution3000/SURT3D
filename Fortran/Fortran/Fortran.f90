df4eecdda34e5240f72b360dddbd0961123883fc
solution3000 <10520284@qq.com>
2016-07-07 17:33:04 +0800

    对变量进行说明，避免编译器报错

--- Fortran/Fortran/Fortran.f90
+++ Fortran/Fortran/Fortran.f90
@@ -20,46 +20,58 @@ contains
         enddo   
     end subroutine
     
+    subroutine test100(n, x, y, z, p,q,r)
+    integer::n
+    !此处声明正确，编译不会报错，计算正确
+    !DEC$ ATTRIBUTES NO_ARG_CHECK :: x,y,z,p,q,r
+    real,dimension(n)::x,y,z,p,q,r
+    do i=1,n
+       p(i)=x(i)+y(i)+z(i)
+       q(i)=x(i)*y(i)*z(i)
+       r(i)=x(i)*y(i)
+    end do  
+    end subroutine 
+   
 end module 
     
 program Fortran
-    
     use mymod
-    
     implicit none
 
-
-    ! Variables
-
     real work(100);
+    real ::x,y,z, p,q,r
     
     call util_integer(1,10, work(1))
     call util_real(20,10,work(1))
     
-    ! Body of Fortran
     print *, 'Hello World'
-       
-    call test2(10,1.0,2.0,3.0,4.0,5.0,6.0)
+
+    !编译报错
+    !call test2(10,1.0,2.0,3.0,4.0,5.0,6.0)
     
-    end program Fortran
+    !调用是对的
+    call test100(1,1.0,2.0,3.0,p,q,r)
+    write(*,*) p, q, r
+    
+end program Fortran
 
     
-    subroutine test1(n, pz,px,py, dvdz,dvdx,dvdy)
-        integer :: n
-        real :: pz(1),px(1),py(1)
-        real :: dvdz(1),dvdx(1), dvdy(1)
-    end subroutine 
+subroutine test1(n, pz,px,py, dvdz,dvdx,dvdy)
+    integer :: n
+    real :: pz(1),px(1),py(1)
+    real :: dvdz(1),dvdx(1), dvdy(1)
+end subroutine 
     
-    subroutine  test2(n, pz, px, py, dvdz, dvdx, dvdy)
-        integer :: n
-        real ::pz(*),px(*),py(*)
-        real :: dvdz(*), dvdx(*), dvdy(*)
-    end subroutine 
+subroutine  test2(n, pz, px, py, dvdz, dvdx, dvdy)
+    integer :: n
+    real ::pz(*),px(*),py(*)
+    real :: dvdz(*), dvdx(*), dvdy(*)
+end subroutine 
     
-    subroutine test3(n, pz, px, py, dvdz, dvdx, dvdy)
-        integer :: n
-        real :: pz(n),px(n),py(n)
-        real :: dvdz(n), dvdx(n), dvdy(n)
-    end subroutine 
+subroutine test3(n, pz, px, py, dvdz, dvdx, dvdy)
+    integer :: n
+    real :: pz(n),px(n),py(n)
+    real :: dvdz(n), dvdx(n), dvdy(n)
+end subroutine 
     
  
