89ead9de8e785dbda0d22ef5ffed7c9582c29689
solution3000 <10520284@qq.com>
2016-07-07 15:41:46 +0800

    解决fortan编译报错问题

--- Fortran/Fortran/Fortran.f90
+++ Fortran/Fortran/Fortran.f90
@@ -1,25 +1,10 @@
-    program Fortran
+   
+module mymod
 
-    implicit none
-
-    ! Variables
-
-    real work(100);
-    
-    call util_integer(1,10, work(1))
-    call util_real(20,10,work(1))
-    
-    
-    ! Body of Fortran
-    print *, 'Hello World'
-    
-    
-    
-    end program Fortran
-
-
+contains
     subroutine  util_integer(ix, n, work)
         integer :: ix, n
+         !DEC$ ATTRIBUTES NO_ARG_CHECK :: work
         integer :: work(1)
         do i=1,n
             work(ix+i-1)=i
@@ -28,8 +13,33 @@
     
     subroutine util_real(ix, n, work)
         integer:: ix, n
+         !DEC$ ATTRIBUTES NO_ARG_CHECK :: work
         real ::work(1)
         do i=1,n
             work(ix+i-1)=0.3
         enddo   
-    end subroutine 
+    end subroutine
+    
+end module 
+    
+program Fortran
+    
+    use mymod
+    
+    implicit none
+
+
+    ! Variables
+
+    real work(100);
+    
+    call util_integer(1,10, work(1))
+    call util_real(20,10,work(1))
+    
+    ! Body of Fortran
+    print *, 'Hello World'
+       
+    
+end program Fortran
+
+ 
