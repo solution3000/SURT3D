3dfb622c1153da7b5821438db2098b760e46916f
solution3000 <10520284@qq.com>
2016-07-08 10:05:21 +0800

    string: c_to_fortran

--- Fortran/Fortran/Fortran.f90
+++ Fortran/Fortran/Fortran.f90
@@ -31,8 +31,38 @@ contains
        r(i)=x(i)*y(i)
     end do  
     end subroutine 
-   
+
 end module 
+
+
+module cstr
+contains
+function c_to_f_string(s) result(str)
+  use iso_c_binding
+  character(kind=c_char,len=1), intent(in) :: s(*)
+  character(len=:), allocatable :: str
+  integer i, nchars
+  i = 1
+  do
+     if (s(i) == c_null_char) exit
+     i = i + 1
+  end do
+  nchars = i - 1  ! Exclude null character from Fortran string
+  allocate(character(len=nchars) :: str)
+  str = transfer(s(1:nchars), str)
+end function c_to_f_string
+
+subroutine pstr(s) bind(c,name='pstr')
+  use iso_c_binding
+  use cstr
+  character(kind=c_char,len=1), intent(in) :: s(*)
+  character(len=:), allocatable :: str
+  integer i, nchars
+  write(*,'(a)') c_to_f_string(s)
+end subroutine pstr
+
+end module cstr
+
     
 program Fortran
     use mymod
